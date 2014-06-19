f4b887458c124bd9c7d32c316b7d982c04984265
courtney mcilhenny <github@courtneymcilhenny>
2014-06-19 09:37:57 -0700

    adding basic code to configure omniauth log in with twitter.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,18 @@
 class User < ActiveRecord::Base
+
+  def self.from_omniauth(auth)
+    where(auth.slice("provider", "uid")).first || create_from_omniauth(auth)
+  end
+
+  def self.create_from_omniauth(auth)
+    create! do |user|
+      user.provider = auth["provider"]
+      user.uid = auth["uid"]
+      user.name = auth["info"]["name"]
+      user.nickname = auth["info"]["nickname"]
+      user.location = auth["info"]["location"]
+      user.image = auth["info"]["image"]
+      user.token = auth["credentials"]["token"]
+    end
+  end
 end
