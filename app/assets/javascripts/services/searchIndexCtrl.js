575380ca307ef7a8b5ff4fbf78a6b551f09817b7
courtney mcilhenny <github@courtneymcilhenny>
2014-06-22 13:44:57 -0700

    adding layout and functionality for working through a user's multiple searches and thier results.

--- app/assets/javascripts/services/searchIndexCtrl.js
+++ app/assets/javascripts/services/searchIndexCtrl.js
@@ -1,5 +1,11 @@
 SearchCtrl = function ($scope, searchData) {
 	$scope.data = searchData.data
+
+	$scope.selectedSearch = null;
+
+	$scope.selectSearch = function (index) {
+		$scope.selectedSearch = index;
+	}
 }
 
 SearchCtrl.$inject = ['$scope', 'searchData'];
