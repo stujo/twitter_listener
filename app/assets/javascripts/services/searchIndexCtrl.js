16021ac0f5c476d58e243690cbb6a50d5cbeab46
courtney mcilhenny <github@courtneymcilhenny>
2014-06-22 12:43:47 -0700

    created a service to handle all search data and populate it with fake data.

--- app/assets/javascripts/services/searchIndexCtrl.js
+++ app/assets/javascripts/services/searchIndexCtrl.js
@@ -1,8 +1,5 @@
-SearchCtrl = function ($scope) {
-	$scope.data = {searches: [{
-		title: "Robots", results: ["Robots are cool, @twitter", "Robots aren't cool, @robotHater", "Like js, Robots are the future @jsLover"]}, {
-		title: "Dumplings", results: ["Yummy dumplings are the best, @kingOfNoodles", "Yummy dumplings are like no other, @dumplingLover", "Yummy dumplings are closed on Thursdays, @sadPanda"]}, {
-		title: "Cats", results: ["Cats are everywhere on the internets, @dogs", "Cats, Cats, Cats... @cats", "Cats are purrrfect, @cat"]}]
-	};
+SearchCtrl = function ($scope, searchData) {
+	$scope.data = searchData.data
 }
 
+SearchCtrl.$inject = ['$scope', 'searchData'];
